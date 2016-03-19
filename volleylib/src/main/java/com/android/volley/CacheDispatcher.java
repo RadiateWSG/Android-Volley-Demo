8f3efa3232e2002f3cc1e4ece4771bea1a389f44
RadiateWSG <wsg1229@qq.com>
2016-03-19 23:20:28 +0800

    对“缓存请求调度线程”的标注

--- volleylib/src/main/java/com/android/volley/CacheDispatcher.java
+++ volleylib/src/main/java/com/android/volley/CacheDispatcher.java
@@ -57,6 +57,15 @@ public class CacheDispatcher extends Thread {
      * @param cache Cache interface to use for resolution
      * @param delivery Delivery interface to use for posting responses
      */
+    /**
+     * @mark
+     * @see RequestQueue#start() 在RequestQueue中的start()方法中被调用
+     * 持有以下引用：
+     * @param cacheQueue 缓存的请求队列
+     * @param networkQueue 网络请求队列
+     * @param cache 缓存的请求结果
+     * @param delivery 请求结果的消息分发器
+     */
     public CacheDispatcher(
             BlockingQueue<Request<?>> cacheQueue, BlockingQueue<Request<?>> networkQueue,
             Cache cache, ResponseDelivery delivery) {
@@ -85,26 +94,31 @@ public class CacheDispatcher extends Thread {
 
         while (true) {
             try {
+                //@mark 从缓存队列中取出一个请求，如果缓存队列为空则进行阻塞。
                 // Get a request from the cache triage queue, blocking until
                 // at least one is available.
                 final Request<?> request = mCacheQueue.take();
                 request.addMarker("cache-queue-take");
 
+                //@mark 如果该请求已经取消了，则从请求队列中删除，并继续取下一条请求。
                 // If the request has been canceled, don't bother dispatching it.
                 if (request.isCanceled()) {
                     request.finish("cache-discard-canceled");
                     continue;
                 }
 
+                //@mark 检查改请求结果是否在缓存结果中，
                 // Attempt to retrieve this item from cache.
                 Cache.Entry entry = mCache.get(request.getCacheKey());
                 if (entry == null) {
                     request.addMarker("cache-miss");
+                    //@mark 如果不在缓存结果中，则加入到网络请求队列中；
                     // Cache miss; send off to the network dispatcher.
                     mNetworkQueue.put(request);
                     continue;
                 }
 
+                //@mark 如果缓存结果已过期，则加入到网络请求队列中。
                 // If it is completely expired, just send it to the network.
                 if (entry.isExpired()) {
                     request.addMarker("cache-hit-expired");
@@ -113,6 +127,7 @@ public class CacheDispatcher extends Thread {
                     continue;
                 }
 
+                //@mark 缓存命中，并调用parseNetworkResponse()方法解析为我们需要的Response。
                 // We have a cache hit; parse its data for delivery back to the request.
                 request.addMarker("cache-hit");
                 Response<?> response = request.parseNetworkResponse(
@@ -120,6 +135,7 @@ public class CacheDispatcher extends Thread {
                 request.addMarker("cache-hit-parsed");
 
                 if (!entry.refreshNeeded()) {
+                    //@mark 缓存命中，且不需要刷新，则直接传递响应的结果。
                     // Completely unexpired cache hit. Just deliver the response.
                     mDelivery.postResponse(request, response);
                 } else {
@@ -132,6 +148,8 @@ public class CacheDispatcher extends Thread {
                     // Mark the response as intermediate.
                     response.intermediate = true;
 
+                    //@mark 需要刷新的情况：传递一个intermediate Response，并带上Runnable参数，
+                    //该Runnable做的操作是把该请求放入网络请求队列中。
                     // Post the intermediate response back to the user and have
                     // the delivery then forward the request along to the network.
                     mDelivery.postResponse(request, response, new Runnable() {
@@ -148,6 +166,7 @@ public class CacheDispatcher extends Thread {
 
             } catch (InterruptedException e) {
                 // We may have been interrupted because it was time to quit.
+                //@mark 此处提供了退出循环的方法。
                 if (mQuit) {
                     return;
                 }
